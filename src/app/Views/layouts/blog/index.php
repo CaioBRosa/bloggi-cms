d10ba967ae686225072f6e3e49ed13789ea159b1
Caio B. Rosa <caiobastos.rosa670@hotmail.com>
2024-02-01 11:03:51 -0300

    Fix: css da pagina contato

--- src/app/Views/layouts/blog/index.php
+++ src/app/Views/layouts/blog/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="<?= base_url('assets/fontawesome/css/all.min.css') ?>">
 	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?= base_url('assets/css/blog.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/blog(new).css') ?>" rel="stylesheet">
 </head>
 <body>
     <?= $this->renderSection('content') ?>
