5caf20fc3cdcc64b3e315d7202f504aaee8e734a
Caio B. Rosa <caiobastos.rosa670@hotmail.com>
2024-02-05 10:59:28 -0300

    Fix: rotas singulares para grupo de rotas

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -6,15 +6,18 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', 'Blog::index');
-$routes->get('/search','Blog::search');
-$routes->get('/post/(:num)', 'Blog::post/$1');
-$routes->get('/sobre', 'Blog::sobre');
-$routes->get('/contato', 'Blog::contato');
+$routes->group('', function ($routes) {
+    $routes->get('/', 'Blog::index');
+    $routes->get('/search','Blog::search');
+    $routes->get('/post/(:num)', 'Blog::post/$1');
+    $routes->get('/sobre', 'Blog::sobre');
+    $routes->get('/contato', 'Blog::contato');
+});
 
 $routes->get('/login','Login::index');
 $routes->post('/login','Login::login');
 $routes->get('/logout','Login::logout');
 
-//dashboard
-$routes->get('/app/dashboard','CMS::index');
+$routes->group('app', function ($routes) {
+    $routes->get('dashboard','CMS::index');
+});
