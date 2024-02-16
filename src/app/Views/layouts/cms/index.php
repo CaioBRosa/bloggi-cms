6e71f9d069a00ef1d650b5d05133678f01a5ccae
Caio B. Rosa <caiobastos.rosa670@hotmail.com>
2024-02-15 22:41:51 -0300

    Cor do background arrumado

--- src/app/Views/layouts/cms/index.php
+++ src/app/Views/layouts/cms/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
 	<meta charset="UTF-8">
 	<title><?= $title ?></title>
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?= base_url('assets/css/dashboard.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/dashboard(new).css') ?>">
 </head>
 <body>
     <?= $this->renderSection('content') ?>
