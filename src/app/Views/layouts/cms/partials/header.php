979101a9361f807e6dde3e128ba0c00552a51062
Caio B. Rosa <caiobastos.rosa670@hotmail.com>
2024-02-15 22:29:54 -0300

    Adaptando dashboard

--- src/app/Views/layouts/cms/partials/header.php
+++ src/app/Views/layouts/cms/partials/header.php
@@ -1,53 +1,112 @@
-<!-- header -->
-    <h1>Lista de Informações</h1>
-    <ul>
-        <li>
-            <a href="<?= base_url('app/dashboard') ?>">Dashboard</a>
-            <ul>
-                <li><a href="#">Visão geral</a></li>
-                <li><a href="#">Estatísticas</a></li>
-                <li><a href="#">Usuários</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= base_url('app/posts') ?>">Posts</a>
-            <ul>
-                <li><a href="#">Criar novo post</a></li>
-                <li><a href="#">Gerenciar posts</a></li>
-                <li><a href="#">Categorias</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= base_url('app/categories') ?>">Categorias</a>
-            <ul>
-                <li><a href="#">Criar nova categoria</a></li>
-                <li><a href="#">Gerenciar categorias</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= base_url('app/comments') ?>">Comentários</a>
-            <ul>
-                <li><a href="#">Aprovar comentários</a></li>
-                <li><a href="#">Gerenciar comentários</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= base_url('app/website') ?>">Website</a>
-            <ul>
-                <li><a href="#">Configurações gerais</a></li>
-                <li><a href="#">Aparência</a></li>
-                <li><a href="#">Plugins</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= base_url('app/settings') ?>">Configurações</a>
-            <ul>
-                <li><a href="#">Perfil</a></li>
-                <li><a href="#">Conta</a></li>
-                <li><a href="#">Notificações</a></li>
-            </ul>
-        </li>
-        <li>
-            <a href="<?= base_url('logout') ?>">logout</a>
-        </li>
-    </ul>
+<aside id="sidebar" class="js-sidebar">
+    <!-- Content For Sidebar -->
+    <div class="h-100">
+        <div class="sidebar-logo">
+            <a href="#">
+                Bloggi - CMS
+                <i class="fa-solid fa-newspaper"></i>
+            </a>
+        </div>
+        <!-- sidebar -->
+        <ul class="sidebar-nav">
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#dashboard" data-bs-toggle="collapse">
+                    <i class="fa-solid fa-gauge-high icon"></i>
+                    Dashboard
+                </a>
+                <ul id="dashboard" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Visão Geral</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Estatisticas</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse" aria-expanded="false">
+                    <i class="fa-solid fa-book icon"></i>
+                    Posts
+                </a>
+                <ul id="posts" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Novo Post</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Gerenciar</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Categorias</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#categories" data-bs-toggle="collapse" aria-expanded="false">
+                    <i class="fa-solid fa-rectangle-list icon"></i>
+                    Categorias
+                </a>
+                <ul id="categories" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Nova Categoria</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Gerenciar</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#comments" data-bs-toggle="collapse" aria-expanded="false">
+                    <i class="fa-solid fa-envelope icon"></i>
+                    Comentarios
+                </a>
+                <ul id="comments" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Aprovar Comentarios</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Gerenciar</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#website" data-bs-toggle="collapse" aria-expanded="false">
+                    <i class="fa-solid fa-blog icon"></i>
+                    Website
+                </a>
+                <ul id="website" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Redes Sociais</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed" data-bs-target="#settings" data-bs-toggle="collapse" aria-expanded="false">
+                    <i class="fa-solid fa-gear icon"></i>
+                    Configurações
+                </a>
+                <ul id="settings" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Perfil</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link sidebar-subcontent">Conta</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="fa-solid fa-right-from-bracket icon"></i>
+                    Logout
+                </a>
+            </li>
+
+        </ul>
+        <!-- sidebar -->
+    </div>
+</aside>
